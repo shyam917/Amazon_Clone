6ff4e9b1170ae4d465cd1e0dcc45cb8ee4ced75e
shyam917 <shyamku917@gmail.com>
2022-01-26 13:25:06 +0530

    continued working on the container in project

--- src/Home.js
+++ src/Home.js
@@ -4,15 +4,16 @@ import "./Product";
 import Product from './Product';
 function Home() {
     return (
-        < div className="home" >
+        <div className="home" >
             <div className="home__container">
                 <img className="home__Image" src="https://m.media-amazon.com/images/I/61jovjd+f9L._SX3000_.jpg" alt="" />
 
                 <div className="home__row">
                     <Product />
+                    <Product />
                 </div>
             </div>
-        </ div >
+        </div >
 
 
     );
--- src/Product.js
+++ src/Product.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import "./Product.css";
 function Product() {
-    return (<div>
-        <div className='product'>
-            <div classname="product__Info">
-                <p>The lean Startup</p>
-                <p className='product__price'>
-                    <small>₹</small>
-                    <strong>300</strong>
-                </p>
-                <div className='product__rating'>
-                    <p>⭐</p>
-                    <p>⭐</p>
-                    <p>⭐</p>
-                </div>
+    return <div className='product'>
+        <div className='product__Info'>
+            <p>The Lean Startup</p>
+            <p className='product__price'>
+                <small>₹</small>
+                <strong>300</strong>
+            </p>
+            <div className='product__rating'>
+                <p>⭐</p>
+                <p>⭐</p>
+                <p>⭐</p>
             </div>
-            <img src="https://m.media-amazon.com/images/I/81RCff1NpnL._AC_UY218_.jpg" alt="" />
-            <button>Add to Cart</button>
         </div>
-    </div>);
+        <img src="https://m.media-amazon.com/images/I/81RCff1NpnL._AC_UY218_.jpg" alt="" />
+        <button>Add to Cart</button>
+    </div>;
 }
 
 export default Product;
