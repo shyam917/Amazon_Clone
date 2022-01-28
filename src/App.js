0191ae21dea79f8b8e2532517c7a35ee1a9481d0
shyam917 <shyamku917@gmail.com>
2022-01-28 21:20:56 +0530

    continued working on the checkout in project

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import "./App.css";
 import React from "react";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     //BEM
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+      <Switch> 
+      <Route path="/checkout">
+            <Header />
+            <h1>checkout</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+         
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
