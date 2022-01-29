4c596bb3b05aaa179dc513285c5ef74c0e5aa01f
shyam917 <shyamku917@gmail.com>
2022-01-29 20:51:59 +0530

    continued working on the checkout page in project

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import React from "react";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Checkout from "./Checkout";
 function App() {
   return (
     //BEM
     <Router>
       <div className="App">
+      <Header />
       <Switch> 
       <Route path="/checkout">
-            <Header />
-            <h1>checkout</h1>
+            <Checkout />
           </Route>
           <Route path="/">
-            <Header />
             <Home />
-          </Route>
-         
+          </Route>        
         </Switch>
       </div>
     </Router>
---
+++ src/Checkout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Checkout.css";
+
+function Checkout() {
+  return( 
+  <div className='checkout'>
+      <div className='checkout__left'>
+          <img className='checkout__ad' src='https://images-eu.ssl-images-amazon.com/images/G/31/img18/Fresh/Promotions/January/BankOffers/JAN21/3_pc.jpg'/>
+      </div>
+  </div>
+  );
+}
+
+export default Checkout;
