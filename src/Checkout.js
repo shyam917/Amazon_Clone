6340c5213767a6f40d8bdebae91e4a40cc0ed5f4
shyam917 <shyamku917@gmail.com>
2022-01-30 21:09:44 +0530

    continued working on the link in project

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,3 +1,4 @@
+import { Divider } from '@material-ui/core';
 import React from 'react';
 import "./Checkout.css";
 
@@ -6,6 +7,12 @@ function Checkout() {
   <div className='checkout'>
       <div className='checkout__left'>
           <img className='checkout__ad' src='https://images-eu.ssl-images-amazon.com/images/G/31/img18/Fresh/Promotions/January/BankOffers/JAN21/3_pc.jpg'/>
+          <div >
+            <h2 className='checkout__title'>your shopping cart</h2>
+          </div>
+      </div>
+      <div className='checkout__right'>
+        <h2>subtotal</h2>
       </div>
   </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { Link } from "react-router-dom";
 function Header() {
   return (
     <div className="header">
+      <Link to="/">   
       <img
         className="header__logo"
         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
       />
+      </Link>
+
       <div className="header__search">
         <input className="header__searchInput" type="text" />
         <SearchIcon className="header__searchIcon" />
@@ -27,12 +31,12 @@ function Header() {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
+        <Link to="/checkout">
         <div className="header__optionCart">
           <ShoppingCartIcon />
           <span className="header__optionLineTwo header__CartCount">0</span>
-
         </div>
-
+        </Link>
       </div>
     </div>
   );
