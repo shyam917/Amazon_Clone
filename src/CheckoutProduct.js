af7888d4cb2f51b05f56496a747d48ce3d177ee9
shyam917 <shyamku917@gmail.com>
2022-02-08 21:47:08 +0530

    product details added

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -6,6 +6,16 @@ function CheckoutProduct({id, title, image, price, rating}) {
         <img className='checkoutProduct__image' src={image}/>  
         <div className='checkoutProduct__info'>
             <p className='checkoutProduct__title'>{title}</p>
+            <p className='checkoutProduct__price'>
+              <small>₹</small>
+              <strong>{price}</strong>
+            </p>
+            <div className='checkoutProduct__rating'>
+              {Array(rating).fill().map((_, i) => (
+                  <p>⭐</p>
+              ))}
+            </div>
+            <button></button>
         </div>
    </div>);
 }
