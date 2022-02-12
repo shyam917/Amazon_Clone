5621b2d975355f8c54f0cd675abd29ddaaa08f07
shyam917 <shyamku917@gmail.com>
2022-02-12 12:23:00 +0530

    error fixed

--- src/Checkout.js
+++ src/Checkout.js
@@ -3,36 +3,42 @@ import React from 'react';
 import "./Checkout.css";
 import { useStateValue } from './Stateprovider';
 import Subtotal from './Subtotal';
+import CheckoutProduct from './CheckoutProduct';
 
 
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ cart }, dispatch] = useStateValue();
   return (
     <div className='checkout'>
       <div className='checkout__left'>
         <img className='checkout__ad' src='https://images-eu.ssl-images-amazon.com/images/G/31/img18/Fresh/Promotions/January/BankOffers/JAN21/3_pc.jpg' />
-        <div >
-          <h2 className='checkout__title'>Your Shopping Cart</h2>
-          {basket.map(item =>
-          (
-            <CheckoutProduct
-              id={item.id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-              rating={item.rating}
-              />
-          )
-
+        <div>
+          <h2 className="checkout__title">Your shopping Cart</h2>
+          {cart.map(item => {
+            console.log(item)
+            return (
+              <CheckoutProduct>
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              </CheckoutProduct>
+            )
+          }
+        )
+          }
         </div>
       </div>
+      {cart?.length > 0 &&
 
       <div className='checkout__right'>
         <Subtotal />
       </div>
+      }
     </div>
-  );
+  )
 }
 
 export default Checkout;
